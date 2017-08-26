cc58312bf51b915abc566f547ef69d5464a053fd
Ricardo EJ <ricardoej@gmail.com>
2017-08-26 12:23:21 -0300

    Ranking the critics based on similarity

--- making_recommendations/recommendations.py
+++ making_recommendations/recommendations.py
@@ -1,72 +1,5 @@
 from math import sqrt
 
-# A dictionary of movie critics and their ratings of a small
-# set of movies
-critics = {
-	'Lisa Rose':
-	{
-		'Lady in the Water': 2.5,
-		'Snakes on a Plane': 3.5,
-		'Just My Luck': 3.0,
-		'Superman Returns': 3.5,
-		'You, Me and Dupree': 2.5,
-		'The Night Listener': 3.0
-	},
-
-	'Gene Seymour':
-	{
-		'Lady in the Water': 3.0,
-		'Snakes on a Plane': 3.5,
-		'Just My Luck': 1.5,
-		'Superman Returns': 5.0,
-		'The Night Listener': 3.0,
-		'You, Me and Dupree': 3.5
-	},
-
-	'Michael Phillips':
-	{
-		'Lady in the Water': 2.5,
-		'Snakes on a Plane': 3.0,
-		'Superman Returns': 3.5,
-		'The Night Listener': 4.0
-	},
-
-	'Claudia Puig':
-	{
-		'Snakes on a Plane': 3.5,
-		'Just My Luck': 3.0,
-		'The Night Listener': 4.5,
-		'Superman Returns': 4.0,
-		'You, Me and Dupree': 2.5
-	},
-
-	'Mick LaSalle':
-	{
-		'Lady in the Water': 3.0,
-		'Snakes on a Plane': 4.0,
-		'Just My Luck': 2.0,
-		'Superman Returns': 3.0,
-		'The Night Listener': 3.0,
-		'You, Me and Dupree': 2.0
-	},
-
-	'Jack Matthews':
-	{
-		'Lady in the Water': 3.0,
-		'Snakes on a Plane': 4.0,
-		'The Night Listener': 3.0,
-		'Superman Returns': 5.0,
-		'You, Me and Dupree': 3.5
-	},
-	
-	'Toby':
-	{
-		'Snakes on a Plane':4.5,
-		'You, Me and Dupree':1.0,
-		'Superman Returns':4.0
-	}
-}
-
 # Return a euclidean distance-based similarity score for person1 and person2
 def euclidean_distance_similarity_score(prefs, person1, person2):
 	# return zero if the two people do not share any recommendation
@@ -84,7 +17,7 @@ def euclidean_distance_similarity_score(prefs, person1, person2):
 def person_correlation_similarity_score(prefs, person1, person2):
 	# get the list of mutually rated items
 	mutually_rated_items = list(set(prefs[person1].keys())
-		.intersection(prefs[person1].keys()))
+		.intersection(prefs[person2].keys()))
 
 	# find the number of mutually rated items
 	n = len(mutually_rated_items)
@@ -110,3 +43,15 @@ def person_correlation_similarity_score(prefs, person1, person2):
 
 	return num / den
 
+# Returns the best matches for person from the prefs dictionary.
+# Number of results and similarity function are optional params.
+def topMatches(prefs, person, n=5, similarity=person_correlation_similarity_score):
+	scores = [(similarity(prefs, person, other), other)
+		for other in prefs if other != person]
+
+	# sort the list so the highest scores appear at the top
+	scores.sort()
+	scores.reverse()
+
+	# return n top results
+	return scores[0 : n]
