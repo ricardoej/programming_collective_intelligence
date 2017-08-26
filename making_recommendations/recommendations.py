52d677bbba8515c5c2a008ad1efb2aa88c7c55fc
Ricardo EJ <ricardoej@gmail.com>
2017-08-26 12:06:42 -0300

    Creating method for calculate similarity score based on Person Correlation

--- making_recommendations/recommendations.py
+++ making_recommendations/recommendations.py
@@ -67,14 +67,46 @@ critics = {
 	}
 }
 
-# Return a distance-based similarity score for person1 and person2
-def distance_similarity_score(prefs, person1, person2):
+# Return a euclidean distance-based similarity score for person1 and person2
+def euclidean_distance_similarity_score(prefs, person1, person2):
 	# return zero if the two people do not share any recommendation
 	if set(prefs[person1].keys()).isdisjoint(prefs[person2].keys()):
 		return 0
-	else:
-		# Add up the squares of all the differences
-		sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item],2)
-			for item in prefs[person1] if item in prefs[person2]])
 
-		return 1 / (1 + sum_of_squares)
+	# add up the squares of all the differences
+	sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item],2)
+		for item in prefs[person1] if item in prefs[person2]])
+
+	# gives higher values for people who are similar
+	return 1 / (1 + sum_of_squares)
+
+# Return a person correlation-based similarity score for person1 and person2
+def person_correlation_similarity_score(prefs, person1, person2):
+	# get the list of mutually rated items
+	mutually_rated_items = list(set(prefs[person1].keys())
+		.intersection(prefs[person1].keys()))
+
+	# find the number of mutually rated items
+	n = len(mutually_rated_items)
+
+	# if they are no ratings in common, return 0
+	if n == 0: return 0
+
+	# add up all the preferences
+	sum1 = sum([prefs[person1][item] for item in mutually_rated_items])
+	sum2 = sum([prefs[person2][item] for item in mutually_rated_items])
+
+	# sum up the squares
+	sum1Sq = sum([pow(prefs[person1][item], 2) for item in mutually_rated_items])
+	sum2Sq = sum([pow(prefs[person2][item], 2) for item in mutually_rated_items])
+
+	# sum up the products
+	pSum = sum([prefs[person1][item] * prefs[person2][item] for item in mutually_rated_items])
+
+	# Calculate Pearson score
+	num = pSum - (sum1 * sum2 / n)
+	den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+	if den == 0: return 0
+
+	return num / den
+
