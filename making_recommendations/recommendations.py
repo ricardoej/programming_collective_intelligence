fa75a6448e1afe46f65b6461e2666f51d98011cc
Ricardo EJ <ricardoej@gmail.com>
2017-08-27 18:02:05 -0300

    Gets recommendations for a person by using a weighted average

--- making_recommendations/recommendations.py
+++ making_recommendations/recommendations.py
@@ -55,3 +55,36 @@ def topMatches(prefs, person, n=5, similarity=person_correlation_similarity_scor
 
 	# return n top results
 	return scores[0 : n]
+
+# Gets recommendations for a person by using a weighted average
+# of every other user's rankings
+def getRecommendations(prefs, person, similarity=person_correlation_similarity_score):
+	totals={}
+	simSums={}
+	
+	for other in prefs:
+		# don't compare me to myself
+		if other != person:
+			sim = similarity(prefs, person, other)
+
+			# ignore scores of zero or lower
+			if sim > 0:
+				for item in prefs[other]:
+					# only score movies I haven't seen yet
+					if item not in prefs[person] or prefs[person][item] == 0:
+						# Similarity * Score
+						totals.setdefault(item, 0)
+						totals[item] += prefs[other][item] * sim
+
+						# Sum of similarities
+						simSums.setdefault(item, 0)
+						simSums[item] += sim
+
+	# Create the normalized list
+	rankings = [(total / simSums[item], item)
+		for item, total in totals.items()]
+
+	# Return the sorted list
+	rankings.sort( )
+	rankings.reverse( )
+	return rankings
