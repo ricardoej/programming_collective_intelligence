0afa949fcda390f4cbf84495166064a2df723f6e
Ricardo EJ <ricardoej@gmail.com>
2017-08-30 19:59:41 -0300

    Changing ratings

--- making_recommendations/movie_ratings.py
+++ making_recommendations/movie_ratings.py
@@ -3,64 +3,64 @@
 ratings = {
 	'Juliana':
 	{
-		'Invocação do Mal 2': 2.5,
+		'Star Trek': 2.5,
 		'Zootopia': 3.5,
-		'Animais Fantásticos e Onde Habitam': 3.0,
-		'Rogue One: Uma História Star Wars': 3.5,
-		'A Grande Muralha': 2.5,
-		'A Lenda de Tarzan': 3.0
+		'Deadpool': 3.0,
+		'Moonlight': 3.5,
+		'A Chegada': 2.5,
+		'Logan': 3.0
 	},
 
 	'Pedro':
 	{
-		'Invocação do Mal 2': 3.0,
+		'Star Trek': 3.0,
 		'Zootopia': 3.5,
-		'Animais Fantásticos e Onde Habitam': 1.5,
-		'Rogue One: Uma História Star Wars': 5.0,
-		'A Lenda de Tarzan': 3.0,
-		'A Grande Muralha': 3.5
+		'Deadpool': 1.5,
+		'Moonlight': 5.0,
+		'Logan': 3.0,
+		'A Chegada': 3.5
 	},
 
 	'Marcelo':
 	{
-		'Invocação do Mal 2': 2.5,
+		'Star Trek': 2.5,
 		'Zootopia': 3.0,
-		'Rogue One: Uma História Star Wars': 3.5,
-		'A Lenda de Tarzan': 4.0
+		'Moonlight': 3.5,
+		'Logan': 4.0
 	},
 
 	'Fabricio':
 	{
 		'Zootopia': 3.5,
-		'Animais Fantásticos e Onde Habitam': 3.0,
-		'A Lenda de Tarzan': 4.5,
-		'Rogue One: Uma História Star Wars': 4.0,
-		'A Grande Muralha': 2.5
+		'Deadpool': 3.0,
+		'Logan': 4.5,
+		'Moonlight': 4.0,
+		'A Chegada': 2.5
 	},
 
 	'Henrique':
 	{
-		'Invocação do Mal 2': 3.0,
+		'Star Trek': 3.0,
 		'Zootopia': 4.0,
-		'Animais Fantásticos e Onde Habitam': 2.0,
-		'Rogue One: Uma História Star Wars': 3.0,
-		'A Lenda de Tarzan': 3.0,
-		'A Grande Muralha': 2.0
+		'Deadpool': 2.0,
+		'Moonlight': 3.0,
+		'Logan': 3.0,
+		'A Chegada': 2.0
 	},
 
 	'Claudio':
 	{
-		'Invocação do Mal 2': 3.0,
+		'Star Trek': 3.0,
 		'Zootopia': 4.0,
-		'A Lenda de Tarzan': 3.0,
-		'Rogue One: Uma História Star Wars': 5.0,
-		'A Grande Muralha': 3.5
+		'Logan': 3.0,
+		'Moonlight': 5.0,
+		'A Chegada': 3.5
 	},
 	
 	'Joaquim':
 	{
 		'Zootopia':4.5,
-		'A Grande Muralha':1.0,
-		'Rogue One: Uma História Star Wars':4.0
+		'A Chegada':1.0,
+		'Moonlight':4.0
 	}
 }
